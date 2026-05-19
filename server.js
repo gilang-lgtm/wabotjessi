541fba03906704c7b00f4605e06340a6cb43ec02
gilang-lgtm <glgpratama6@gmail.com>
2026-05-19 23:42:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -683,8 +683,6 @@ app.get('/api/sessions/status/:code', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-
-    console.log('Server running on port ' + PORT);
-
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running');
 });
