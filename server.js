b4429293bc73df0fb276a0f4dd31914fc77fe48a
gilang-lgtm <glgpratama6@gmail.com>
2026-06-12 13:25:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -47,12 +47,6 @@ async function downloadExcel() {
 
        const latestResponse = await axios.get(
     LATEST_URL + '?t=' + Date.now(),
-    {
-        responseType: 'text',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36'
-        }
-    }
 );
 console.log('TYPE:', typeof latestResponse.data);  // ← tambah ini
 console.log('DATA:', latestResponse.data);     
